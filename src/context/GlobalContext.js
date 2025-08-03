57f4aad3ae0539eac3eccbc166ac6e8d5e87d3d9
CarloDettori <dettoric07@gmail.com>
2025-08-03 20:17:58 +0200

    assets

--- src/context/GlobalContext.js
+++ src/context/GlobalContext.js
@@ -5,12 +5,12 @@ const GlobalProvider = ({ children }) => {
 
 
     const postsData = use(fetch("/api/posts").then(res => res.json()));
-    const [posts, setPosts]
+    const [posts, setPosts] = useState(postsData)
 
 
 
     return (
-        <GlobalContext.Provider value={{ posts, setPosts, favourites, setFavourites }}>
+        <GlobalContext.Provider value={{ posts, setPosts }}>
             {children}
         </GlobalContext.Provider>
     )
